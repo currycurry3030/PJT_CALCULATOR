5093acc2ff7b51dfa1c641c07b90b78d76cc10d2
jong2.ha <jong2.ha@samsung.com>
2024-07-10 16:55:00 +0900

    [Feature] SumSum, TestCase 추가.

--- calc.py
+++ calc.py
@@ -2,4 +2,4 @@ class Calc:
     pass
 
     def get_sum_sum(self, a, b, c):
-        return 1 + 2 + 3
+        return a + b + c
--- test_calc.py
+++ test_calc.py
@@ -10,5 +10,7 @@ class TestCalc(TestCase):
 
     def test_get_sum_sum(self):
         self.assertEqual(self.calc.get_sum_sum(1, 2, 3), 6)
-
-
+        self.assertEqual(self.calc.get_sum_sum(2, 3, 4), 9)
+        self.assertEqual(self.calc.get_sum_sum(-3, 3, 8), 8)
+        self.assertEqual(self.calc.get_sum_sum(1, -13, -20), -32)
+        self.assertEqual(self.calc.get_sum_sum(73, 13, -103), -17)
