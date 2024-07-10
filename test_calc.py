e1619ce4b4a55f73bde1952ffba069b9dd698168
jwh1308 <67500007+jwh1308@users.noreply.github.com>
2024-07-10 16:48:05 +0900

    [add] getGop

--- test_calc.py
+++ test_calc.py
@@ -1,5 +1,9 @@
 from unittest import TestCase
+
 from calc import Calc
 
+
 class TestCalc(TestCase):
-    pass
+    def test_getGop(self):
+        self.calc = Calc()
+        self.assertEqual(12, self.calc.getGop(3, 4))
