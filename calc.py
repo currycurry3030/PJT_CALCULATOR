7f8fd931cf86080d42c31d4b17265d0857cb23dc
jong2.ha <jong2.ha@samsung.com>
2024-07-10 16:46:16 +0900

    Ready

--- calc.py
+++ calc.py
@@ -1,2 +1,6 @@
 class Calc:
     pass
+
+
+    def get_sum_sum(self,a,b):
+        pass
--- test_calc.py
+++ test_calc.py
@@ -3,3 +3,8 @@ from calc import Calc
 
 class TestCalc(TestCase):
     pass
+
+
+    def test_get_sum_sum(self):
+        pass
+
