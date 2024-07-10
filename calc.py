3689fcd178e2e9fc407a62902e73ff36243853b1
jong2.ha <jong2.ha@samsung.com>
2024-07-10 16:49:25 +0900

    [Feature] 1 - test_get_sum_sum

--- calc.py
+++ calc.py
@@ -1,6 +1,5 @@
 class Calc:
     pass
 
-
-    def get_sum_sum(self,a,b):
-        pass
+    def get_sum_sum(self, a, b, c):
+        return 1 + 2 + 3
--- test_calc.py
+++ test_calc.py
@@ -1,10 +1,14 @@
 from unittest import TestCase
 from calc import Calc
 
+
 class TestCalc(TestCase):
     pass
 
-
     def test_get_sum_sum(self):
-        pass
+        calc = Calc()
+
+        result = calc.get_sum_sum(1, 2, 3)
+
+        self.assertEqual(result, 6)
 
