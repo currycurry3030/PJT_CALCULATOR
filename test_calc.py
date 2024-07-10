97a6268c0a15e68bdfeb5c0c9e585531f43069ff
bj93.park <qazws9346@gmail.com>
2024-07-10 16:51:04 +0900

    [FEAT] Add getSum

--- test_calc.py
+++ test_calc.py
@@ -2,4 +2,15 @@ from unittest import TestCase
 from calc import Calc
 
 class TestCalc(TestCase):
-    pass
+    def setUp(self):
+        super().setUp()
+        self.calc = Calc()
+
+    def test_get_sum(self):
+        lst_pair_number = [(1, 2, 3),
+                           (300, 8, 308),
+                           (1.3, 8, 9.3),
+                           (1.2, 2.3, 3.5)]
+        for (number_1, number_2, ans) in lst_pair_number:
+            ret = self.calc.getSum(number_1,number_2)
+            self.assertEqual(ans, ret)
