f11dc106ddb3eb3f9d9ec4b6d7449530b4383a0c
jwh1308 <67500007+jwh1308@users.noreply.github.com>
2024-07-10 16:51:26 +0900

    [add] getZegop

--- test_calc.py
+++ test_calc.py
@@ -1,5 +1,9 @@
 from unittest import TestCase
+
 from calc import Calc
 
+
 class TestCalc(TestCase):
-    pass
+    def test_get_zegop(self):
+        self.calc = Calc()
+        self.assertEqual(81, self.calc.getZegop(9))
