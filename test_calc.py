0b247a6d9c17c4d99fa58965cd19c75a32fb9ca3
sumin1s.song <sumin1s.song@samsung.com>
2024-07-10 17:57:44 +1000

    [getMinus] getMius 기능 추가

--- test_calc.py
+++ test_calc.py
@@ -2,4 +2,16 @@ from unittest import TestCase
 from calc import Calc
 
 class TestCalc(TestCase):
-    pass
+
+    def setUp(self):
+        super().setUp()
+        self.calc = Calc()
+
+    def test_getMinus_1_2(self):
+        self.assertEqual(-1, self.calc.getMinus(1, 2))
+
+    def test_getMinus_2_1(self):
+        self.assertEqual(1, self.calc.getMinus(2, 1))
+
+
+
